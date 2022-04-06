ddefc885ef7337cf243480044e8d2557161a2a33
Danielle Ismerim <danismerim@hotmail.com>
2022-04-06 21:10:22 +0200

    [DAEM-166] Add ToggleButton component to switch between absolute and percentage amount for MM-base-script

--- frontend/src/components/new-script-page/blocks/actions/mmBaseAction.tsx
+++ frontend/src/components/new-script-page/blocks/actions/mmBaseAction.tsx
@@ -5,12 +5,24 @@ import { IToken } from '../../../../data/tokens';
 import { TokensModal } from "../shared/tokens-modal";
 import { ToggleButtonField } from '../shared/toggle-button';
 import { BaseMoneyMarketActionType } from '@daemons-fi/shared-definitions';
+import { AmountType } from '@daemons-fi/shared-definitions/build';
 
 
-const amountValidation = (value: string) => {
-    if (!value || value === '') return 'required';
-    if (Number(value) <= 0) return 'required > 0';
-    return undefined;
+const validateForm = (values: IBaseMMActionForm) => {
+    const errors: any = {};
+    if (!values.floatAmount || (values.floatAmount as any) === '') {
+        errors.floatAmount = 'required';
+    }
+    if (values.floatAmount && Number(values.floatAmount) <= 0) {
+        errors.floatAmount = 'required > 0';
+    }
+    return errors;
+};
+
+const isFormValid = (values: IBaseMMActionForm) => {
+    const errors = validateForm(values);
+    const isValid = Object.keys(errors).length === 0;
+    return isValid;
 };
 
 export const MmBaseAction = ({ form, update }: { form: IBaseMMActionForm; update: (next: IBaseMMActionForm) => void; }) => {
@@ -29,33 +41,35 @@ export const MmBaseAction = ({ form, update }: { form: IBaseMMActionForm; update
     return (
         <Form
             initialValues={form}
+            validate={validateForm}
             onSubmit={() => { /** Individual forms are not submitted */ }}
-            render={({ handleSubmit, valid }) => (
+            render={({ handleSubmit }) => (
                 <form onSubmit={handleSubmit}>
 
                     <div className='transfer-block'>
-                        <div className="script-block__panel--row">
+                        <div className="script-block__panel--two-columns">
                             <ToggleButtonField
                                 name='actionType'
                                 valuesEnum={BaseMoneyMarketActionType}
                                 updateFunction={(newValue) => { update({ ...form, actionType: newValue }); }}
                                 initial={form.actionType}
                             />
-                        </div>
-
-                        <div className="script-block__panel--two-columns">
-
-                            <TokensModal
-                                tokens={tokens}
-                                selectedToken={selectedToken}
-                                setSelectedToken={setSelectedToken}
+                            <ToggleButtonField
+                                name='amountType'
+                                valuesEnum={AmountType}
+                                updateFunction={(newValue: AmountType) => {
+                                    const updatedForm = { ...form, amountType: newValue, floatAmount: (newValue === AmountType.Percentage ? 50 : 0) };
+                                    const valid = isFormValid(updatedForm);
+                                    update({ ...updatedForm, valid });
+                                }}
+                                initial={form.amountType}
                             />
-
+                        </div>
+                        {form.amountType === AmountType.Absolute ?
                             <Field name="floatAmount"
                                 component="input"
                                 type="number"
                                 placeholder='1.00'
-                                validate={amountValidation}
                             >
                                 {({ input, meta }) =>
                                     <input
@@ -64,24 +78,49 @@ export const MmBaseAction = ({ form, update }: { form: IBaseMMActionForm; update
                                         onChange={(e) => {
                                             e.target.value = Number(e.target.value) < 0 ? '0' : e.target.value;
                                             input.onChange(e);
-                                            update({ ...form, floatAmount: Number(e.target.value) });
-                                        }}
-                                        onBlur={(e) => {
-                                            input.onBlur(e);
-                                            update({ ...form, valid });
+                                            const updatedForm = { ...form, floatAmount: Number(e.target.value) };
+                                            const valid = isFormValid(updatedForm);
+                                            update({ ...updatedForm, valid });
                                         }}
                                         placeholder="Amount"
                                     />
                                 }
-                            </Field>
+                            </Field> :
+                            (<div className='slider-container'>
+                                <Field name="floatAmount"
+                                    component="input"
+                                    type="range"
+                                >
+                                    {({ input, meta }) =>
+                                        <input
+                                            min="50"
+                                            max="10000"
+                                            step="50"
+                                            {...input}
+                                            className={`${meta.error ? 'script-block__field--error' : null}`}
+                                            onChange={(e) => {
+                                                input.onChange(e);
+                                                const updatedForm = { ...form, floatAmount: Number(e.target.value) };
+                                                const valid = isFormValid(updatedForm);
+                                                update({ ...updatedForm, valid });
+                                            }}
+                                        />
+                                    }
+                                </Field>
 
+                                <div className='slider-container__slider-value'>
+                                    {`${form.floatAmount / 100}%`}
+                                </div>
+                            </div>)
+                        }
+
+                        <div className="script-block__panel--two-columns">
+                            <TokensModal
+                                tokens={tokens}
+                                selectedToken={selectedToken}
+                                setSelectedToken={setSelectedToken}
+                            />
                         </div>
-                        <div>
-                            TODO
-                            <ul>
-                                <li>Add amount type toggle (absolute/percentage)</li>
-                            </ul>
-                        </div >
                     </div >
                 </form>
             )}
