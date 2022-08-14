5e661d0f2fe74a0859e8b06977bd3e32800fea31
floredickinson <alvinsartor@gmail.com>
2022-08-14 21:59:58 +0000

    Admin tested and deployed

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ dotenv.config();
 if (!process.env.ADMIN_WALLET_PRIVATE_KEY) throw new Error("ADMIN_WALLET_PRIVATE_KEY was not set");
 
 if (!process.env.KOVAN_RPC) throw new Error("KOVAN_RPC was not set");
-if (!process.env.FANTOM_TESTNET_RPC) throw new Error("FANTOM_TESTNET_RPC was not set");
 if (!process.env.MUMBAI_RPC) throw new Error("MUMBAI_RPC was not set");
 
 export const app = express();
